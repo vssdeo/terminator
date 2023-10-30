dfcd477aff6dc21083c03db2479e6b29ac03d7a5
Vishweshwar Saran Singh Deo <vssdeo@gmail.com>
2023-10-30 21:28:25 +0530

    new-selecting-text-with-keyboard-shift-up-shift-down
    - added select all copy to clipboard without window
    - cleaned up the code and formatting
    - updated with latest plugins, changes and bug fixes

--- terminatorlib/plugins/mousefree_select.py
+++ terminatorlib/plugins/mousefree_select.py
@@ -15,37 +15,38 @@ import terminatorlib.plugin as plugin
 from terminatorlib.plugin import KeyBindUtil
 
 from terminatorlib.util import err, dbg, gerr
-from terminatorlib import regex
-
-import re
 
 
 AVAILABLE = ['MouseFreeKeyTextSelect']
 
-PluginKbSelActKeyUp = "plugin_kbsel_key_up"
-PluginKbSelActKeyDn = "plugin_kbsel_key_dn"
-PluginKbSelActEsc   = "plugin_kbsel_esc"
+PluginKbSelActKeyUp     = 'plugin_kbsel_key_up'
+PluginKbSelActKeyDn     = 'plugin_kbsel_key_dn'
+PluginKbSelActKeySelAll = 'plugin_kbsel_sel_all'
+PluginKbSelActEsc       = 'plugin_kbsel_esc'
 # we use the same as in terminal for consistency
-PluginKbSelActCp    = "copy"
+PluginKbSelActCp        = 'copy'
 
 
-PluginKbSelKeyUp = "Plugin Select Text Up"
-PluginKbSelKeyDn = "Plugin Select Text Dn"
-PluginKbSelEsc   = "Plugin Clear/Esc Selected Text"
-PluginKbSelCp    = "Plugin Copy Selected Text"
+PluginKbSelKeyUp    = 'Plugin Select Text Up'
+PluginKbSelKeyDn    = 'Plugin Select Text Dn'
+PluginKbSelKeySelAll= 'Plugin Select Text All'
+PluginKbSelEsc      = 'Plugin Clear/Esc Selected Text'
+PluginKbSelCp       = 'Plugin Copy Selected Text'
 
-PluginKbSelLabelDesc =  "(Read-Only) Copy Paste Clipboard Buffer\
-   Select: %s   Copy: %s   Cancel: %s"
+PluginKbSelLabelDesc=  '(Read-Only) Copy Paste Clipboard Buffer\
+   Select: %s   Copy: %s   Cancel: %s'
 
 
 class MouseFreeKeyTextSelect(plugin.Plugin):
+
+    capabilities = ['MouseFreeHandler']
+
     from_column = 0
     from_row    = 0
     to_column   = 0
     to_row      = 0
     vte         = None
-    select_on   = False
-    terminal    = None
+    cur_term    = None
 
     config      = Config()
     keyb        = KeyBindUtil(config)
@@ -55,44 +56,84 @@ class MouseFreeKeyTextSelect(plugin.Plugin):
         self.connect_signals()
 
         self.keyb.bindkey_check_config(
-                [PluginKbSelKeyUp , PluginKbSelActKeyUp, "<Shift>Up"])
+            [PluginKbSelKeyUp, PluginKbSelActKeyUp, '<Shift>Up'])
         self.keyb.bindkey_check_config(
-                [PluginKbSelKeyDn , PluginKbSelActKeyDn, "<Shift>Down"])
+            [PluginKbSelKeyDn, PluginKbSelActKeyDn, '<Shift>Down'])
+        self.keyb.bindkey_check_config(
+            [PluginKbSelKeySelAll, PluginKbSelActKeySelAll, '<Ctrl>a'])
+
 
     def connect_signals(self):
         self.windows = Terminator().get_windows()
         for window in self.windows:
             window.connect('key-press-event', self.on_keypress)
 
-    def unload(self):
-        for window in self.windows:
-            window.disconnect_by_func(self.on_keypress)
+        for term in Terminator().terminals:
+            dbg("signal connect term:%s" % term)
+            term.connect('focus-in', self.on_focus_in)
 
-    def get_term(self):
-        return  Terminator().last_focused_term
+
+    def unload(self):
+        self.keyb.unbindkey(
+            [PluginKbSelKeyUp, PluginKbSelActKeyUp, '<Shift>Up'])
+        self.keyb.unbindkey(
+            [PluginKbSelKeyDn, PluginKbSelActKeyDn, '<Shift>Down'])
+        self.keyb.unbindkey(
+            [PluginKbSelKeySelAll, PluginKbSelActKeySelAll, '<Ctrl>a'])
+
+        #bound below
+        self.keyb.unbindkey(
+                [PluginKbSelEsc , PluginKbSelActEsc,'Escape'])
+        self.keyb.unbindkey(
+                [PluginKbSelCp, PluginKbSelActCp,   '<Control><Shift>c'])
+
+
+        for term in Terminator().terminals:
+            try:
+                term.disconnect_by_func(self.on_focus_in)
+            except:
+                dbg("no connected signals")
+
+        for window in self.windows:
+            try:
+                window.disconnect_by_func(self.on_keypress)
+            except:
+                dbg("no connected signals")
+
+
+    #TODO: remove when bug is fixed in Terminator()
+    def get_focussed_terminal(self):
+        """iterate over all the terminals to find which, if any, has focus"""
+        for terminal in Terminator().terminals:
+            if terminal.get_vte().has_focus():
+                return(terminal)
+        return(None)
+
+
+    def on_focus_in(self, widget, event = None):
+        dbg("focus-in: %s" % widget)
+        self.cur_term = self.get_focussed_terminal()
+        self.vte      = self.cur_term.get_vte()
+
 
     def close_select_text(self):
-        self.select_on = False
-        dbg("select_start: %s" % self.select_on)
         self.window.close()
 
-    def start_select_text(self):
-        dbg("start select text")
-        self.select_on = True
 
-        self.terminal = self.get_term()
-        self.vte = self.get_term().get_vte()
+    def start_select_text(self):
+        dbg('start select text')
 
         window = Gtk.Window()
 
-        # these won't be shown in keybindings as these are enabled
-        # when clipboard buffer window is show, we could create a
-        # local copy of keybind
+        # these shouldn't be shown in keybindings as these are enabled
+        # when clipboard buffer window is shown, we could create a
+        # local copy of keybind or have a feature in keybind TODO:
+        # TODO: these should be unbound on window close
 
         self.keyb.bindkey_check_config(
-                [PluginKbSelEsc , PluginKbSelActEsc,"Escape"])
+                [PluginKbSelEsc , PluginKbSelActEsc,'Escape'])
         self.keyb.bindkey_check_config(
-                [PluginKbSelCp, PluginKbSelActCp, "<Control><Shift>c"])
+                [PluginKbSelCp, PluginKbSelActCp, '<Control><Shift>c'])
 
         # get key-combo for copy action
         cp_key_str =  self.keyb.get_act_to_keys_config(PluginKbSelActCp)
@@ -101,17 +142,17 @@ class MouseFreeKeyTextSelect(plugin.Plugin):
         sel_esc_str = self.keyb.get_act_to_keys(PluginKbSelActEsc)
 
         title = PluginKbSelLabelDesc % ( sel_up_str +
-                            "/" + sel_dn_str, cp_key_str, sel_esc_str)
+                            '/' + sel_dn_str, cp_key_str, sel_esc_str)
         window_frame = Gtk.Frame(label=title)
-        window_frame.modify_bg(Gtk.StateType.NORMAL, Gdk.color_parse("red"))
+        window_frame.modify_bg(Gtk.StateType.NORMAL, Gdk.color_parse('red'))
 
         window.add(window_frame)
 
-        window.set_title("Clipboard ~ Copy Paste")
-        (width, height) = self.terminal.get_toplevel().get_size()
+        window.set_title('Clipboard ~ Copy Paste')
+        (width, height) = self.cur_term.get_toplevel().get_size()
         window.set_default_size(width, height)
 
-        parent_x, parent_y = self.terminal.get_toplevel().get_position()
+        parent_x, parent_y = self.cur_term.get_toplevel().get_position()
         window.move(parent_x + 50, parent_y)
 
         scrolled_win = Gtk.ScrolledWindow()
@@ -121,7 +162,7 @@ class MouseFreeKeyTextSelect(plugin.Plugin):
         self.from_column, self.from_row = (0,0)
         self.to_column, self.to_row     = self.vte.get_cursor_position()
 
-        #dbg("text to %s %s" % (self.to_column, self.to_row))
+        #dbg('text to %s %s' % (self.to_column, self.to_row))
         txtbuf = Gtk.TextBuffer()
         txt = self.vte.get_text_range_format(Vte.Format.TEXT,
                                              self.from_row, self.from_column,
@@ -135,7 +176,7 @@ class MouseFreeKeyTextSelect(plugin.Plugin):
         textview.set_monospace(True)
 
         it_end = txtbuf.get_end_iter()
-        mark_end = txtbuf.create_mark("", it_end, False)
+        mark_end = txtbuf.create_mark('', it_end, False)
         textview.scroll_to_mark(mark_end, 0, False, 0, 0)
 
         textview.grab_focus()
@@ -143,9 +184,9 @@ class MouseFreeKeyTextSelect(plugin.Plugin):
         agr = Gtk.AccelGroup()
         window.add_accel_group(agr)
         act_str = self.keyb.get_all_act_to_keys()[PluginKbSelActCp]
-        dbg("action string: %s" % act_str)
-        key, mod = Gtk.accelerator_parse("<Shift><Control>c")
-        textview.add_accelerator("copy-clipboard",
+        dbg('action string: %s' % act_str)
+        key, mod = Gtk.accelerator_parse('<Shift><Control>c')
+        textview.add_accelerator('copy-clipboard',
                     agr, key, mod, Gtk.AccelFlags.VISIBLE)
 
         scrolled_win.add(textview)
@@ -154,25 +195,36 @@ class MouseFreeKeyTextSelect(plugin.Plugin):
         self.textview = textview
         self.window   = window
 
-    def on_copy_clipboard(self, widget):
+
+    def on_sel_all_copy_clipboard(self):
+        self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        col, row =  self.vte.get_cursor_position()
+        (txt, attr) = self.vte.get_text_range_format(
+                                Vte.Format.TEXT, 0, 0, row, col)
+        self.clipboard.set_text(txt, -1)
+
+
+    def on_copy_clipboard(self, widget, event = None):
         self.window.close()
         self.textview = None
         self.window   = None
 
+
     #buffer window event handler
     def on_clipboard_keypress(self, widget, event):
         act = self.keyb.keyaction(event)
-        dbg("clipboard keyaction: (%s) (%s)" % (str(act), event.keyval))
+        dbg('clipboard keyaction: (%s) (%s)' % (str(act), event.keyval))
         if act == PluginKbSelActEsc:
             self.close_select_text()
 
         if act == PluginKbSelActCp:
             pass
 
+
     #plugin keypress handler
     def on_keypress(self, widget, event):
         act = self.keyb.keyaction(event)
-        dbg("keyaction: (%s) (%s)" % (str(act), event.keyval))
+        dbg('keyaction: (%s) (%s)' % (str(act), event.keyval))
 
         if act == PluginKbSelActKeyUp:
             self.start_select_text()
@@ -180,6 +232,10 @@ class MouseFreeKeyTextSelect(plugin.Plugin):
 
         if act == PluginKbSelActKeyDn:
             self.start_select_text()
+            return True
+
+        if act == PluginKbSelActKeySelAll:
+            self.on_sel_all_copy_clipboard()
             return True
 
         if act == PluginKbSelActEsc:
